f70f996bcaf5051ba0e1c8949316ef57756eb3cb
DESKTOP-B44NIUN\caicai <aihaohao920@yahoo.com>
2021-08-07 15:48:31 +0800

    修改相关依赖

--- cp-gateway/src/main/java/top/caicai/gatewayApplication.java
+++ cp-gateway/src/main/java/top/caicai/gatewayApplication.java
@@ -1,11 +1,11 @@
 package top.caicai;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy //开启网关
-@SpringCloudApplication //springbooot 应用
+@EnableZuulProxy//开启网关
+@SpringBootApplication //springbooot 应用
 public class gatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(gatewayApplication.class);
