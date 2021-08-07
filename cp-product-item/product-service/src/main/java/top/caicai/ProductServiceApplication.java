b22422295c024097a7995679fb7dce7ee26abcd0
DESKTOP-B44NIUN\caicai <aihaohao920@yahoo.com>
2021-08-07 23:11:16 +0800

    解决版本依赖冲突

---
+++ cp-product-item/product-service/src/main/java/top/caicai/ProductServiceApplication.java
@@ -0,0 +1,12 @@
+package top.caicai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class);
+    }
+}
